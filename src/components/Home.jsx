ea04b9ab7d32d3b23f503697ddfa09b222faa0c8
Hannah Farrell <hmholland3@catamount.wcu.edu>
2024-08-29 13:44:50 -0400

    Worked on Homepage

---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './Home.css';
+
+class Home extends React.Component{
+
+
+    render() {
+        return <div>
+            <article>
+                <div className="container">
+                    <div className="container-inner">
+                        <div className = "image">
+                        <img src="/images/melol.jpg" alt="Me"/>
+                        </div>
+                        <h3 className="Welcome">Hi, Hello, and welcome to my photography
+                            portfolio!</h3>
+                        <p className="About">
+                            Here you will find some of my favorite images, located on the Portfolio
+                            page.
+                            On the Albums page (To be completed!) You will find albums of images
+                            from my
+                            most recent adventures. If you are interested, you can contact me using
+                            the
+                            Contact Me page. Happy Scrolling!
+                        </p>
+                    </div>
+                </div>
+            </article>
+        </div>
+
+    }
+}
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "./Navbar.css";
+import styled from "styled-components";
+
+const StyledText = styled.h1`
+    font-family: "Monsterrat", sans-serif;
+    font-size: 30px;
+`;
+
+/**
+ *
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Navbar(){
+    return <nav className="nav">
+        <div className="nav__title">
+            <h4 className="title">Hannah Farrell Photography</h4>
+        </div>
+        <ul className="nav__list">
+            <li className="nav__item"><Link to="/">Home</Link></li>
+            <li className="nav__item"><Link to="portfolio">Portfolio</Link></li>
+            <li className="nav__item"><Link to="albums">Albums</Link></li>
+            <li className="nav__item"><Link to="contact-me">Contact Me</Link></li>
+        </ul>
+    </nav>
+
+}
+
+export default Navbar
